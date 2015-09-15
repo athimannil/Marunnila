ee20715c82a6d373ab0501b135badf5fc0b42936
Muhammed Athimannil <hello@athimannil.com>
2015-09-15 23:22:34 +0400

    remove favorite item with click

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,7 +4,7 @@ angular.module('starter.controllers', [])
   // our array
   var savedfav = [2, 4, 7];
   // storing our array as a string
-  localStorage.setItem("savefavorite", JSON.stringify(savedfav));
+  localStorage.setItem("savedFav", JSON.stringify(savedfav));
 })
 
 .controller('aboutCtrl', function($scope) {
@@ -37,15 +37,41 @@ angular.module('starter.controllers', [])
   $scope.yesFav = false;
 
   // retrieving our data and converting it back into an array
-  var retrievedData = localStorage.getItem("savefavorite");
+  var retrievedData = localStorage.getItem("savedFav");
   var myfavs = JSON.parse(retrievedData);
   //making sure it still is an array
-  console.log(myfavs.length);
-  console.log(myfavs);
+  /*console.log(myfavs.length);
+  console.log(myfavs);*/
 
   $scope.isFav = function(id){
-    if(myfavs.indexOf(id) != -1){
+    if(checkExist(id)){
       $scope.yesFav = true;
+    }
+  };
+  function checkExist(id) {
+    if(myfavs.indexOf(id) != -1){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  $scope.addtofav = function(itemId){
+
+    var bank = myfavs.indexOf(itemId);
+    console.log(bank);
+
+    // localStorage.setItem('names_length', myfavs.length);
+    if(checkExist(itemId)){
+      // Delete from favlist
+      myfavs.splice(bank, 1);
+      localStorage.setItem("savedFav", JSON.stringify(myfavs));
+      $scope.yesFav = false;
+    }else{
+    // var savedfav = [2, 4, 7];
+    // storing our array as a string
+
+      console.log("Not yet favorited");
     }
   };
 })
